1d6770b1fa954529eda50ef7f739c108a8eb8b06
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-03 15:50:36 -0300

    feat: Adding complete entry_test.

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,73 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alvarolucio2007/TheBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomFixedAccountEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	return entry
+}
+
+func createRandomEntry(t *testing.T) Entry {
+	randomAccount := createRandomAccount(t)
+	arg := CreateEntryParams{
+		AccountID: randomAccount.ID,
+		Amount:    util.RandomMoney(),
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.CreatedAt, entry2.CreatedAt)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	randomAccount := createRandomAccount(t)
+	for range 10 {
+		createRandomFixedAccountEntry(t, randomAccount)
+	}
+	arg := ListEntriesParams{
+		AccountID: randomAccount.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
