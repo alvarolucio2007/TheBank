2e4c27c15487d6e50f1fa73f1eff6c44a9258040
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-03 15:09:06 -0300

    feat: Adding incomplete account_test

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alvarolucio2007/TheBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+	arg := CreateAccountParams{
+		Owner:   user.Username,
+		Balance: util.RandomMoney(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+
+	require.NotZero(t, account.ID)
+
+	require.NotZero(t, account.CreatedAt)
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
