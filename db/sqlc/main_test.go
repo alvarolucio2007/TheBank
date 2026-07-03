05090c02f12b41a71a208776df1fe67cf0853cdd
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-03 16:14:03 -0300

    fix: Making testDB local variable into global

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -21,7 +21,7 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal("Cannot get the file: ", err)
 	}
-	testDB, err := sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the DB: ", err)
 	}
