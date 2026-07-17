5b3713e1f5c5e6d557b5dd6f8d969838db1d7320
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-17 17:02:43 -0300

    feat: Fixing entry point.

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/alvarolucio2007/TheBank/api"
+	db "github.com/alvarolucio2007/TheBank/db/sqlc"
+	"github.com/alvarolucio2007/TheBank/util"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server, err := api.NewServer(store)
+	if err != nil {
+		log.Fatal("cannot instance new server:", err)
+	}
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
