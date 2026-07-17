f6c2fd0ffd5c7484f15c31ec9685a232c9611b1a
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-17 01:11:17 -0300

    feat: Adding incomplete account file, only getAccount and createAccount.

---
+++ api/account.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/alvarolucio2007/TheBank/db/sqlc"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createAccountRequest struct {
+	Owner string `json:"owner" binding:"required"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.CreateAccountParams{
+		Owner:   req.Owner,
+		Balance: 0,
+	}
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusOK, account)
+	}
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
