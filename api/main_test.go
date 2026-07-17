27552b0fb830c4a9974d7deb853bbe57ec9eae00
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-17 17:00:28 -0300

    feat: Implementing newTestServer

--- api/main_test.go
+++ api/main_test.go
@@ -4,8 +4,17 @@ import (
 	"os"
 	"testing"
 
+	db "github.com/alvarolucio2007/TheBank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	server, err := NewServer(store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
