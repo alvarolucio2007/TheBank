ed586c027f446cbfb3b399a04b95c4134e52a930
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-17 23:19:49 -0300

    feat: Adapting handlers and tests for paseto auth

--- api/transfer.go
+++ api/transfer.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/alvarolucio2007/TheBank/db/sqlc"
+	"github.com/alvarolucio2007/TheBank/token"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +22,16 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
+	fromAccount, valid := server.validAccount(ctx, req.FromAccountID)
+	if !valid {
+		return
+	}
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("from account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 	arg := db.TransferTxParams{
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
@@ -30,4 +43,17 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountID int64) (db.Account, bool) {
+	account, err := server.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return account, false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return account, false
+	}
+	return account, true
 }
