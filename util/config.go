3f374b24e60757b15e35e8c6edfe13577b7349a3
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-31 10:08:02 -0300

    fix: Ajusting DBSource mapstructure to DB_SOURCE and adjusting for AWS compatibility.

--- util/config.go
+++ util/config.go
@@ -8,7 +8,7 @@ import (
 
 type Config struct {
 	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_URL"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
 	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
 	TokenSymnetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
@@ -16,12 +16,22 @@ type Config struct {
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
+	viper.SetConfigName("important")
 	viper.SetConfigType("env")
+
+	viper.BindEnv("DB_DRIVER", "DB_DRIVER")
+	viper.BindEnv("DB_SOURCE", "DB_SOURCE")
+	viper.BindEnv("SERVER_ADDRESS", "SERVER_ADDRESS")
+	viper.BindEnv("TOKEN_SYMMETRIC_KEY", "TOKEN_SYMMETRIC_KEY")
+	viper.BindEnv("ACCESS_TOKEN_DURATION", "ACCESS_TOKEN_DURATION")
 
 	viper.AutomaticEnv()
 	if err = viper.ReadInConfig(); err != nil {
-		return
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			err = nil
+		} else {
+			return
+		}
 	}
 	if err = viper.Unmarshal(&config); err != nil {
 		return
